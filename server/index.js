10d30ed92b33a70fc16604197ee9a6b6e92b76a8
Alexander Huerta <alexhuerta305@gmail.com>
2021-12-19 12:59:52 -0500

    Created project manifest. Downloaded some dependencies. Began to create a skeleton for the prject( client, server and database directories)

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3080 || process.env.PORT;
+
+app.use(express.static('client/dist'));
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+})
