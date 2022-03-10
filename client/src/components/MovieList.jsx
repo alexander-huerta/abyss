abeb4697290b642588da5ce2f91c60ef2a2a56fa
Alexander Huerta <alexhuerta305@gmail.com>
2022-03-09 22:04:32 -0500

    Added drop down menu for sorting by genre

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
+
+
 
 function MovieList ({movies}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
     return (
-    <ImageList >
-      {movies.map((movie) => (
-        <ImageListItem key={movie.id}>
-          <h3> {movie.title}</h3>
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            alt={movie.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-  </ImageList>
+      <ImageList >
+        {movies.map((movie) => (
+          <ImageListItem key={movie.id}>
+            <h3> {movie.title}</h3>
+            <StarOutlineOutlinedIcon/>x
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.title}
+              loading="lazy"
+              onClick={()=>console.log('open modal')}
+            />
+
+          </ImageListItem>
+
+        ))}
+    </ImageList>
   )
 }
 
 export default MovieList;
+
+
+
 
