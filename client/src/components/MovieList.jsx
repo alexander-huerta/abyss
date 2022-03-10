73f11a354f2e35de5f5a8c6c1bce22c3a9602afc
Alexander Huerta <alexhuerta305@gmail.com>
2022-03-09 19:42:29 -0500

    deleted MovieListItem component

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import MovieListEntry from './MovieListEntry.jsx';
-
-
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
-
 function MovieList ({movies}) {
-  // return (
-  //   <div>
-  //     <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-  //     {movies.map((movie) =>
-  //     <ImageListItem
-  //     key={src}
-  //     style={{ margin: '2px', width: 'calc(100% / 5 - 4.2px)'>
-  //        <MovieListEntry
-  //          movie={movie}
-  //          key={movie.id}/>
-  //          </ImageListItem>
-  //     )}
-  //      </ImageList>
-  //   </div>
-  // )
-
-
     return (
     <ImageList >
       {movies.map((movie) => (
