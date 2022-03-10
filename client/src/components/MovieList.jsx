99e4281a632e6a5c06b849741c29780b58fce082
Alexander Huerta <alexhuerta305@gmail.com>
2022-03-10 10:43:54 -0500

    added title bar overlay and resized images

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from 'react';
+
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
 import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
 
 
-
 function MovieList ({movies}) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
     return (
-      <ImageList >
+      <ImageList cols={3} gap={30}>
         {movies.map((movie) => (
-          <ImageListItem key={movie.id}>
-            <h3> {movie.title}</h3>
-            <StarOutlineOutlinedIcon/>x
+          <ImageListItem sx={{ width: 300, height: 300 }} key={movie.id}>
             <img
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.title}
               loading="lazy"
-              onClick={()=>console.log('open modal')}
             />
-
+            <ImageListItemBar
+              title={movie.title}
+              subtitle={movie.id}
+              actionIcon={
+            <IconButton
+              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+              aria-label={`info about ${movie.title}`}
+              onClick={() => console.log('open modal')}
+            >
+            <InfoIcon />
+            </IconButton>
+            }
+          />
           </ImageListItem>
-
         ))}
     </ImageList>
   )
